b5c2f6428e3ad03c5e4c24f54f93dea6502c93f4
sonramos <jackson.ramoos@outlook.com>
2023-11-16 22:17:45 -0300

    initial modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from "./shared/material/material.module";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { CompanyComponent } from './company/company.component';
+import { PopupComponent } from './popup/popup.component';
+import {MatTableModule} from "@angular/material/table";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyComponent,
+    PopupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/model/company.ts
@@ -0,0 +1,8 @@
+export interface Company {
+  id: number,
+  name: string,
+  phone: string,
+  address: string,
+  manager: string,
+  isActive: boolean
+}
