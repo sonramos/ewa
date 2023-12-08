abac19225de65ae3469049fca01d364e3239bb3a
Jackson Ramos <jackson.ramoos@outlook.com>
2023-12-07 23:48:45 -0300

    pipe phone rules

--- src/app/shared/pipes/phone.pipe.ts
+++ src/app/shared/pipes/phone.pipe.ts
@@ -7,6 +7,14 @@ export class PhonePipe implements PipeTransform {
 
   transform(value: string): string {
     // (12)34567-8900
-    return `(${value.substring(0,2)})${value.substring(2,7)}-${value.substring(7,11)}`;
+    if (value.length == 10) {
+      return `(${value.substring(0,2)})${value.substring(2,6)}-${value.substring(6,10)}`;
+    }
+    else if (value.length == 11) {
+      return `(${value.substring(0, 2)})${value.substring(2, 7)}-${value.substring(7, 11)}`;
+    }
+    else {
+      return value;
+    }
   }
 }
